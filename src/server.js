8694112bf7b15c457c6e106a6db1dedf3673f5fb
kevinrisqi <kevinrisqi22@gmail.com>
2022-06-29 20:33:46 +0700

    create server

---
+++ src/server.js
@@ -0,0 +1,14 @@
+/* eslint-disable eol-last */
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 3000,
+    host: 'localhost',
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
